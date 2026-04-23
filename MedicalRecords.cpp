9ccbfb25d3e7c85cb3241f8937ebd54d88d9b194
MueezAshfaq <mueezashfaq098@gmail.com>
2026-04-23 23:03:45 +0500

    Added Functions for Setting Records and input validations

--- MedicalRecords.cpp
+++ MedicalRecords.cpp
@@ -11,7 +11,6 @@ MedicalRecords::MedicalRecords() {
 	treatmentCost = 0;
 	date = "1/1/2000";
 }
-//hello world
 MedicalRecords::MedicalRecords(string recID, string patID, string docID, string diag, string t, double tcost, string d) {  //string t is for treatment(used like this everywhere else as well)
 	recordId = recID;
 	patientId = patID;
@@ -20,6 +19,16 @@ MedicalRecords::MedicalRecords(string recID, string patID, string docID, string 
 	treatment = t;
 	treatmentCost = tcost;
 	date = d;
+}
+//Helper Function
+string MedicalRecords::generateRecordId()const {
+	recordCounter++;
+	string num = to_string(recordCounter);
+	while (num.length() < 4) {
+		num = "0" + num;
+	}
+	string id = "R-" + num;
+	return id;
 }
 //Validating Record ID
 bool MedicalRecords::isValidRecordId(string id) {
@@ -37,7 +46,7 @@ bool MedicalRecords::isValidRecordId(string id) {
 	}
 	return true;
 }
-bool MedicalRecords::recordIdAlreadyExists(string filename, string id) {
+bool MedicalRecords::recordIdAlreadyExists(string id,string filename) {
 	ifstream infile(filename);
 	if (infile.is_open()) {
 		string sep,rId;
@@ -54,6 +63,85 @@ bool MedicalRecords::recordIdAlreadyExists(string filename, string id) {
 		return false;
 	}
 	return false;
+}
+//Validating Date
+bool MedicalRecords::isLeapYear(int year) const {
+	return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+}
+bool MedicalRecords::isValidDate(string dt) {
+	int firstSlash=dt.find('/');
+	if (firstSlash == string::npos)	//npos means NOT FOUND
+		return false;
+	int secondSlash = dt.find('/',firstSlash+1);
+	if (secondSlash == string::npos)	
+		return false;
+	int date = stoi(dt.substr(0,firstSlash));
+	int month = stoi(dt.substr(firstSlash+1,secondSlash-firstSlash-1));
+	int year = stoi(dt.substr(secondSlash+1));
+	if (year < 1900 || year>2100)
+		return false;
+	if (month < 1 || month>12)
+		return false;
+	if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+		if (date < 1 || date>31)
+			return false;
+	}
+	else if (month == 4 || month == 6 || month == 9 || month == 11) {
+		if (date < 1 || date>30)
+			return false;
+	}
+	else {
+		if (isLeapYear(year)) {
+			if (date < 1 || date>29)
+				return false;
+		}
+		else {
+			if (date < 1 || date>28)
+				return false;
+		}
+	} 
+	return true;
+}
+void MedicalRecords::setMedicalRecords(Person* currentUser) {
+	if (currentUser->Get_Role() == "doctor") {
+		recordId = generateRecordId();
+		Doctor* doc = dynamic_cast<Doctor*>(currentUser);
+		doctorId = doc->getDoctorId();
+		while (true) {
+			cout << "Enter Patient ID (format P-0001)" << endl;
+			cin >> patientId;
+			if (!isValidPatientId(patientId)) {
+				cout << "Invalid Patient ID.Try Again" << endl;
+			}
+			else if (!patientIdAlreadyExists(patientId, filename)) {
+				cout << "Invalid! Patient ID Already Exists.Try Again" << endl;
+			}
+			else {
+				break;
+			}
+		}	
+		cin.ignore();
+		cout << "Enter Diagnoses" << endl;
+		getline(cin,diagnoses);
+		cout << "Enter Treatment" << endl;
+		getline(cin, treatment);
+		cout << "Enter Treatment Cost" << endl;
+		cin >> treatmentCost;
+		while (treatmentCost < 0) {
+			cout << "Invalid input! Try Again" << endl;
+			cin >> treatmentCost;
+		}
+		cin.ignore();
+		while (true) {
+			cout << "Enter Date (format 1/1/2000)" << endl;
+			getline(cin, date);
+			if (!isValidDate(date)) {
+				cout << "Invalid! Try Again" << endl;
+			}
+			else
+				break;
+		}
+	}
 }
 //Getter Functions
 string MedicalRecords::getRecordId()const {
@@ -99,7 +187,7 @@ void MedicalRecords::setTreatmentCost(double tcost) {
 void MedicalRecords::setDate(string d) {
 	date = d;
 }
-//When calling from main call this one 1 time and then however many records using display function
+//When calling from main call this one 1 time and then the normal display function
 void MedicalRecords::displayHeader() {
 	cout << left;
 	cout<< setw(12) << "RecordID"<< setw(12) << "PatientID"<< setw(12) << "DoctorID"<< setw(15) << "Diagnoses"<< setw(15) << "Treatment"<< setw(10) << "Cost"<< setw(12) << "Date" << endl;
@@ -117,8 +205,28 @@ void MedicalRecords::display() const {
 	cout << right;
 }
 //File Handling
+void MedicalRecord::loadCounterFromFile(string filename) {		//Static Function
+	ifstream infile(filename);
+	int maxnum = 0;
+	int num = 0;
+	string sep, rId;
+	if (infile.is_open()) {
+		while (getline(infile, sep)) {
+			getline(infile, rId);
+			int num = stoi(rId.substr(2));				//stoi converts string to int
+			if (maxnum < num)
+				maxnum = num;
+			string skip;								//Skipping the rest(Only need RecordID)
+			for (int i = 0; i < 6; i++) {
+				getline(infile, skip);
+			}
+		}
+		infile.close();
+	}
+	recordCounter = maxnum;
+}
 void MedicalRecords::saveToFile(string filename)const {
-	ofstream filename(filename, ios::app);
+	ofstream outfile(filename, ios::app);
 	if (outfile.is_open()) {
 		outfile << "-----------" << endl;
 		outfile << recordId << endl;
@@ -145,6 +253,7 @@ void MedicalRecords::loadFromFile(string filename) {
 		getline(infile,date);
 	}
 }
+//----Managing Records-----
 //Search Functions
 void MedicalRecords::searchByRecordId(string filename,string id) {
 	ifstream infile(filename);
@@ -237,4 +346,12 @@ void MedicalRecords::searchByDoctorId(string filename, string id) {
 	}
 	cout << "Record Not Found" << endl;
 }
+//Update/Delete Records
+void MedicalRecords::updateRecords() {
 
+}
+void MedicalRecords::deleteRecords() {
+
+}
+MedicalRecords::~MedicalRecords(){}
+int MedicalRecords::recordCounter = 0;		//IMP NOTE:IN MAIN DO THIS----> MedicalRecords::loadCounterFromFile("MedicalRecords.txt")
--- MedicalRecords.h
+++ MedicalRecords.h
@@ -13,36 +13,46 @@ private:
 	double treatmentCost;
 	string date;
 	public:
+		static int recordCounter;
 	MedicalRecords();
 	MedicalRecords(string recID,string patID,string docID,string diag,string t,double tcost,string d);
+	string generateRecordId()const;
 	bool isValidRecordId(string id);
-	bool recordIdAlreadyExists(string id, string filename);
+	bool recordIdAlreadyExists(string filename, string id);
+	bool isLeapYear(int year) const;
+	bool isValidDate(string dt);
+	void setMedicalRecords(Person* currentUser);
 	//Getters
-	string getRecordsId()const;
+	string getRecordId()const;
 	string getPatientId()const;
 	string getDoctorId()const;
-	string getDiagnoses()const
+	string getDiagnoses()const;
 	string getTreatment()const;
 	string getTreatmentCost()const;
 	string getDate()const;
 	//Setters
-	void setRecordsId(string id);
+	void setRecordId(string id);
 	void setPatientId(string id);
 	void setDoctorId(string id);
 	void setDiagnoses(string id);
 	void setTreatment(string id);
-	void setTreatmentCost(string id);
+	void setTreatmentCost(double tcost);
 	void setDate(string id);
 	//Display
-	void DisplayHeader();
-	void display();
+	void displayHeader();
+	void display()const;
 	//File Handling
+	static void loadCounterFromFile(string filename);
 	void saveToFile(string filename)const;
 	void loadFromFile(string filename);
 	//Search Functions
 	void searchByRecordId(string filename, string id);
 	void searchByPatientId(string filename, string id);
 	void searchByDoctorId(string filename, string id);
+	//Update/Delete Records
+	void updateRecords();
+	void deleteRecords();
+	~MedicalRecords();
 };
 
 
